888f3c6d501d5895f69d545e989ff1b88878d7e0
matt <matt@protectamerica.com>
2016-12-28 17:53:41 -0600

    adds new tests updates method name

--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -29,3 +29,21 @@ def test_rule_parser_creates_anti_rule():
 	}
 	rp.parse(rule)
 	assert_equal(antiParsed, rp.antiParsed)
+
+def test_rule_parser_creates_parsed_must_size_rule():
+	rule = 'must contain 1 large'
+	parsed = {
+		'must': ['large'],
+		'cannot': []
+	}
+	rp.parse(rule)
+	assert_equal(parsed, rp.parsed)
+
+def test_rule_parser_creates_parsed_cannot_size_rule():
+	rule = 'cannot contain 1 large'
+	antiParsed = {
+		'must': [],
+		'cannot': ['large']
+	}
+	rp.parse(rule)
+	assert_equal(antiParsed, rp.parsed)
--- zendo/parser.py
+++ zendo/parser.py
@@ -6,23 +6,23 @@ class RuleParser(object):
 		self.parsed = self.parse_rule(rule[3], qty, type=rule[0])
 		self.antiParsed = self.parse_anti_rule(rule[3], qty, type=rule[0])
 
-	def parse_rule(self, color, qty, type):
+	def parse_rule(self, attr, qty, type):
 		pr = {'must':[], 'cannot': []}
-		pr[type] = self.add_colors(color, qty)
+		pr[type] = self.add_attrs(attr, qty)
 		return pr
 
-	def parse_anti_rule(self, color, qty, type):
+	def parse_anti_rule(self, attr, qty, type):
 		pr = {'must':[], 'cannot': []}
 		if(type == 'must'):
 			type = 'cannot'
 		else:
 			type = 'must'
-		pr[type] = self.add_colors(color, qty)
+		pr[type] = self.add_attrs(attr, qty)
 		return pr
 
-	def add_colors(self, color, qty):
+	def add_attrs(self, attr, qty):
 		colors = []
 		while qty > 0:
-			colors.append(color)
+			colors.append(attr)
 			qty-=1
 		return colors
